5844cb3e52d85f40da559f1b89ca184b7e040116
karennz23 <karennz023@gmail.com>
2016-12-09 00:06:09 -0600

    Me frustran las '

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+var unidades=[{key:1,value:"CENAPRECE"},{key:2,value:"CENSIA"},{key:3,value:"CENSIDA"},{key:4,value:"CNEGSR"},{key:5,value:"DGE"},{key:6,value:"DGPS"},{key:7,value:"STCONAPRA"},{key:8,value:"STCONSAME"}]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tablero SPPS' });
+  res.render('index', { title: 'Tablero SPPS',unidades:unidades });
 });
 
 module.exports = router;
