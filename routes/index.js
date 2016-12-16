a020fc06a1d964e3e756d72a38b0a42037770ab8
Mario Torres <mario291187@gmail.com>
2016-12-15 18:17:04 -0600

    Ejemplo de consulta

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var pgp = require('pg-promise')();
 
-var unidades=[{key:1,value:"CENAPRECE"},{key:2,value:"CENSIA"},{key:3,value:"CENSIDA"},{key:4,value:"CNEGSR"},{key:5,value:"DGE"},{key:6,value:"DGPS"},{key:7,value:"STCONAPRA"},{key:8,value:"STCONSAME"}]
+var cn = {
+    host: 'localhost',
+    //port: 5433,
+    database: 'karen',
+    user: 'mtorres',
+    password: 'test'
+};
+
+var db = pgp(cn);
+
+//var unidades=[{key:1,value:"CENAPRECE"},{key:2,value:"CENSIA"},{key:3,value:"CENSIDA"},{key:4,value:"CNEGSR"},{key:5,value:"DGE"},{key:6,value:"DGPS"},{key:7,value:"STCONAPRA"},{key:8,value:"STCONSAME"}]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tablero SPPS',unidades:unidades });
+
+  db.manyOrNone('select distinct(unidad) from pae',[]).then(function ( data ) {
+
+      res.render('index', { title: 'Tablero SPPS',unidades: data });
+
+  }).catch(function (error) {
+      console.log(error);
+  });
 });
 
 module.exports = router;
