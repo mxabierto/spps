d49fc1451b2f5d420f3cfe9ddca8481f72e24074
Carlos Cessa <carlos@pixative.com>
2017-02-27 16:16:53 -0600

    LeafLet Map
    - Modified map visualization to use the LeafLet library and select the
    first indicator and latest year by default

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,196 +1,144 @@
-var map ;
-function main() {
-    cartodb.createVis('map', 'https://karennz23.carto.com/api/v2/viz/2101ccca-eca3-11e6-977d-0e3ff518bd15/viz.json', {
-        shareable: true,
-        title: false,
-        description: true,
-        search: true,
-        tiles_loader: true,
-        center_lat: 24,
-        center_lon: -99,
-        zoom: 5,
-        position: 'center'
-
-    }).done(function (vis, layers) {
-        map = vis.getNativeMap();
-    }).error(function (error) {
-        console.log(err);
-    });
-    /*  .done(function(vis, layers) {        https://karennz23.carto.com/builder/731e06d0-d629-11e6-902c-0ecd1babdde5/embed<--Mapa de estados
-     // layer 0 is the base layer, layer 1 is cartodb layer
-     // setInteraction is disabled by default
-     layers[1].setInteraction(true);
-     layers[1].on('featureOver', function(e, latlng, pos, data) {
-     cartodb.log.log(e, latlng, pos, data);
-     })
-     // getting the native map to work with it
-     var map = vis.getNativeMap();
-     //creating sublayers
-     cartodb.createLayer(map, {
-     user_name: 'karennz23',
-     type: 'cartodb',
-     sublayers: [
-     {
-     sql: "SELECT * FROM entidades where cov_id=2",
-     cartocss: '#entidades {polygon-fill: #ff2200;}'
-     },
-     {
-     sql: "SELECT * FROM entidades where cov_id=3",
-     cartocss: '#entidades {polygon-fill: #00d667;}'
-     }
-     ]
-     }).addTo(map)
-     })
-     .error(function(err) {
-     console.log(err);
-     });*/
-
-}
-window.onload = main;
-
+var map;
+var states;
+var years_data;
+var geojson_states;
 
 //selects
-var select_unidad = $('#select_unidad');
-var select_pae = $('#select_pae');
-var select_indicador = $('#select_indicador');
+var select_unidad       = $( '#select_unidad' );
+var select_pae          = $( '#select_pae' );
+var select_indicador    = $( '#select_indicador' );
 
-var ficha = $('#ficha');
-var tabla_ind = $('#tabla_indicador');
-var map_pagination = $('#anios');
+var ficha               = $( '#ficha' );
+var tabla_ind           = $( '#tabla_indicador' );
+var map_pagination      = $( '#anios' );
+
+function select_unidad_cb () {
+    select_pae.load( '/select-pae', {
+        unidad  : $( this ).val()
+    }, _paeSelected );
+};
 
 function color( val ) {
     switch ( val ){
         case 0:
-            return '#565656';//'#a65fc6';
+            return '#565656';
             break;
         case 1:
-            return '#ff0000 ';
+            return '#ff0000';
             break;
         case 2:
-            return '#faff00';//'#fff547';
+            return '#faff00';
             break;
         case 3:
-            return '#28b72d';// '#00d667';
+            return '#28b72d';
             break;
         case 4:
-            return '#ffa323';//'#ffb351';
+            return '#ffa323';
             break;
         case 5:
             return '#42a1f4';
             break;
     }
 
-    return '#edeff2';//'grey';
+    return '#edeff2';
 }
 
-select_unidad.change(function () {
-    select_pae.load('/select-pae', { unidad : $(this).val() }, function () {
-        select_indicador.load('/select-ficha', { id_pae : $(this).val() }, function () {
-            //Intenta cargar la ficha de un indicador
-            ficha.load('/ficha', {id_ficha: select_indicador.val()});
-            tabla_ind.load('/tabla-indicador', {id_ficha: select_indicador.val()});
-            map_pagination.html('');
-            map_pagination.load('/anios', { id : select_indicador.val() },function () {
-                //eventos
-                $('.pagination').find('li').click(function () {
-                    //alert( $(this).data('id_ficha'));
-                    $.post('/colores', { id : $(this).data('id_ficha'), anio: $(this).data('anio') }, function(data){
-                        for (var i=0; i< data.length; i++){
-                            pintar(data[i].id, color (data[i].color ));
-                        }
-                    });
-                });
-            });
+select_unidad.change( select_unidad_cb );
 
-        });
+select_pae.change( _paeSelected );
+
+select_indicador.change( _indicatorSelected );
+
+function _indicatorSelected () {
+    ficha.load( '/ficha', {
+        id_ficha    : select_indicador.val()
     });
-});
-
-//Evento para cargar el listado de indicadores
-select_pae.change(function () {
-    //Carga el listado de indicadores en el el select
-    select_indicador.load('/select-ficha', { id_pae: $(this).val() }, function () {
-        //Intenta cargar la ficha de un indicador
-        ficha.load('/ficha', {id_ficha: select_indicador.val()});
-        tabla_ind.load('/tabla-indicador', {id_ficha: select_indicador.val()});
-        map_pagination.html('');
-        map_pagination.load('/anios', { id : select_indicador.val() },function () {
-            //eventos
-            $('.pagination').find('li').click(function () {
-                //alert( $(this).data('id_ficha'));
-                $.post('/colores', { id : $(this).data('id_ficha'), anio: $(this).data('anio') }, function(data){
-                    for (var i=0; i< data.length; i++){
-                        pintar(data[i].id, color (data[i].color ));
-                    }
-                });
-            });
-        });
-
-    });
-});
-
-// Actualiza la ficha al cambiar la opción de indicador
-select_indicador.change(function () {
-    //Intenta cargar la ficha de un indicador
-    ficha.load('/ficha',{ id_ficha: $(this).val()});
-    tabla_ind.load('/tabla-indicador',{ id_ficha: $(this).val()});
-    map_pagination.html('');
-    map_pagination.load('/anios', { id : select_indicador.val() },function () {
-        //eventos
-        $('.pagination').find('li').click(function () {
-            //alert( $(this).data('id_ficha'));
-            $.post('/colores', { id : $(this).data('id_ficha'), anio: $(this).data('anio') }, function(data){
-                for (var i=0; i< data.length; i++){
-                    pintar(data[i].id, color (data[i].color ));
-                }
-            });
-        });
+    tabla_ind.load( '/tabla-indicador', {
+        id_ficha    : select_indicador.val()
     });
 
-});
-
-/*Pintar estados*/
-
-function pintar (i, color) {
-//clear map
-
-    cartodb.createLayer(map, {
-        user_name: 'karennz23',
-        type: 'cartodb',
-        sublayers: [
-            {
-                sql: "SELECT cartodb_id,the_geom_webmercator FROM entidades where cov_id="+i,
-                cartocss: '#entidades {polygon-fill: '+color+'; ::outline {line-width: 2;line-opacity: 0.5;}}'
-            }
-        ]
-    }).addTo(map)
+    _setPagination();
 }
-/*Pintar jurisdicciones*/
-/*
- function  pintar(i, color) {
- //clear map
- cartodb.createLayer(map, {
- user_name: 'karennz23',
- type: 'cartodb',
- sublayers: [
- {
- sql: "SELECT cartodb_id,the_geom_webmercator FROM js_prueba where clave_juri="+i,
- cartocss: '#js_prueba {polygon-fill: '+color+';::outline {line-width: 1;line-opacity: 0.5;}}'
- }
- ]
- }).addTo(map)
- }*/
-/*Pintar municipios*//*
- function pintar (i, color) {
- //clear map
- cartodb.createLayer(map, {
- user_name: 'karennz23',
- type: 'cartodb',
- sublayers: [
- {
- sql: "SELECT * FROM municipal_juris where cvegeo="+i,
- cartocss: '#municipal_juris {polygon-fill: '+color+';line-width: 2;line-opacity: 0.5;}'
- }
- ]
- }).addTo(map)
- }*/
+
+function _paeSelected () {
+    select_indicador.load( '/select-ficha', {
+        id_pae  : select_pae.val()
+    }, _indicatorSelected );
+}
+
+function _setPagination () {
+    map_pagination.html( '' );
+    map_pagination.load( '/anios', {
+        id : select_indicador.val()
+    }, function () {
+        $( '.pagination' ).find( 'li' ).click( function () {
+            $( '.pagination li' ).removeClass( 'active' );
+            var pag_el  = $( this );
+            pag_el.addClass( 'active' );
+            $.post( '/colores', {
+                id      : pag_el.data( 'id_ficha' ),
+                anio    : pag_el.data( 'anio' )
+            }, function( data ){
+                years_data  = data;
+                draw_states();
+            });
+        });
+
+        var items   = $( '.pagination li' );
+        $( items.get( items.length - 2 ) ).click();
+    });
+}
+
+function _onFeature ( feature, drawnLayer ) {
+    var el  = _.findWhere( years_data, {
+        nombre  : feature.properties.NOM_ENT
+    });
+
+    drawnLayer.setStyle({
+        color       : '#ffffff',
+        opacity     : 1,
+        weight      : 1,
+        fillColor   : color( el && el.color || -1 ),
+        fillOpacity : 1
+    });
+}
+
+function draw_states () {
+    if ( geojson_states ) {
+        map.removeLayer( geojson_states );
+    }
+
+    geojson_states  = L.geoJson( states, {
+        onEachFeature   : function ( feature, layer ) {
+            _onFeature( feature, layer );
+        }
+    });
+
+    geojson_states.addTo( map );
+}
+
+function main() {
+    $.get( '/data/states_topo.json', function ( data ) {
+        states  = topojson.feature( data, data.objects.states );
+    });
+
+    map   = L.map( 'map', {
+        minZoom         : 5,
+        maxZoom         : 6,
+        scrollWheelZoom : false
+    }).setView( [ 24.127, -102 ], 5 );
+    L.tileLayer( 'http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution     : 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+    }).addTo( map );
+
+    // Select the first item in the `Unidad Administrativa` dropdown
+    $( 'option', select_unidad ).each( function () {
+        if ( this.value ) {
+            select_unidad.val( this.value );
+            select_unidad_cb.apply( select_unidad[0] );
+            return false;
+        }
+    });
+}
+
+$( document ).ready( main );
